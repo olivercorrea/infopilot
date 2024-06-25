25a8a0e64a9d1523842d64e95a6304f38c1cc91d
Dronexx3 <fabian.hilares@tecsup.edu.pe>
2024-06-24 22:17:57 -0500

    validators en el login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,34 +11,44 @@ import { AuthGoogleService } from '../auth-google.service';
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  showPassword: boolean = false;
   isLogin: boolean = true;
   erroMessage: string = "";
-  constructor(private router: Router,private http: HttpClient, private authGoogleService: AuthGoogleService) {}
+
+  constructor(private router: Router, private http: HttpClient, private authGoogleService: AuthGoogleService) {}
+
   login() {
+    if (!this.email || !this.password) {
+      alert("Por favor, complete todos los campos.");
+      return;
+    }
+
     console.log(this.email);
     console.log(this.password);
     let bodyData = {
       email: this.email,
       password: this.password,
     };
-        this.http.post("http://localhost:9992/user/login", bodyData).subscribe(  (resultData: any) => {
-        console.log(resultData);
-        if (resultData.status) 
-        {
-          if (resultData.token) {
-            localStorage.setItem('token', resultData.token);
-            this.router.navigateByUrl('/home');
-          }
-        } 
-        else
-         {
-          alert("Incorrect Email or Password");
-          console.log("Errror login");
+
+    this.http.post("http://localhost:9992/user/login", bodyData).subscribe((resultData: any) => {
+      console.log(resultData);
+      if (resultData.status) {
+        if (resultData.token) {
+          localStorage.setItem('token', resultData.token);
+          this.router.navigateByUrl('/home');
         }
-      });
-    }
-  
+      } else {
+        alert("Correo electrónico o contraseña incorrectos.");
+        console.log("Error en el login");
+      }
+    });
+  }
+
   googlelogin() {
     this.authGoogleService.login();
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
 }
